47c940228eb1db330d3698d3a3a9f78d8d74b822
developerpark2348 <developerpark2348@gmail.com>
2021-01-17 16:28:56 +0900

    typescript class-note update
    튜플 (배열의 인덱스마다 타입을 지정함)
    let address: [string, number] = ['gangnam', 100];
    
    객체
    let obj: object = {};
    
    let person: { name: string, age: number } = { name: 'thor', age: 1000}
    
    진위값(true, false)
    let show: boolean = true;

--- class-note/1_type_basic.ts
+++ class-note/1_type_basic.ts
@@ -9,5 +9,26 @@ let num: number = 10;
 
 // Ts 배열<변수형>
 let arr: Array<number> = [1,2,3];
+
 let heroes: Array<string> = ['Captain', 'Thor', 'Hulk']
+
 let items: number[] = [1,2,3]
+
+// Ts 튜플 (배열의 인덱스마다 타입을 지정함)
+let address: [string, number] = ['gangnam', 100];
+
+// Ts 객체
+let obj: object = {};
+
+// let person: object = {
+//     name: 'capt',
+//     age: 1000
+// };
+
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000
+}
+
+// Ts 진위값(true, false)
+let show: boolean = true;
