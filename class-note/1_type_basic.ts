3ef635d84deed1ce776bc634298cd000a1ed1378
developerpark2348 <developerpark2348@gmail.com>
2021-01-17 16:19:20 +0900

    typescript class-note
    let str: string => 문자열
    let num: number => 숫자형
    let arr: Array<타입> => 배열<타입>형
    let arr: 타입[] => <타입>형 배열

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,13 @@
+// Js 문자열 선언
+// var str = 'hello';
+
+// Ts 문자열 선언
+let str: string = 'hello';
+
+// Ts 숫자
+let num: number = 10;
+
+// Ts 배열<변수형>
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['Captain', 'Thor', 'Hulk']
+let items: number[] = [1,2,3]
