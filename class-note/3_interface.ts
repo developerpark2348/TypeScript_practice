38183d92c844e2556ccb08cace53ae9669aeb0bc
developerpark2348 <developerpark2348@gmail.com>
2021-01-18 15:31:10 +0900

    함수의 구조에 인터페이스를 활용
    라이브러리 + 협업시 유용함.

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -21,3 +21,13 @@ const capt = {
 }
 
 getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+var sum: SumFunction;
+sum = function(a: number, b: number) {
+    return a + b;
+}
